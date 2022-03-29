602791498c9e006e5c1aaa33defef900bf0a754f
de11ete <99339619+de11ete@users.noreply.github.com>
2022-03-30 01:20:38 +0400

    Update Main.java

--- lesson2/Main.java
+++ lesson2/Main.java
@@ -1,45 +1,57 @@
 package lesson2;
 
 public class Main {
+    public static int arraySum(String[][] twoDimensionalArray) throws MyArraySizeException, MyArrayDataException {
+        if (twoDimensionalArray.length != 4) {
+            throw new MyArraySizeException("Длина массива != 4");
+        }
+
+        for (String[] array : twoDimensionalArray) {
+            if (array.length != 4) {
+                throw new MyArraySizeException("Высота массива не равна 4");
+            }
+        }
+
+        int sum = 0;
+        for (int i = 0; i < twoDimensionalArray.length; i++) {
+            for (int j = 0; j < twoDimensionalArray[i].length; j++) {
+                try {
+                    sum += Integer.parseInt(twoDimensionalArray[i][j]);
+                } catch (NumberFormatException e) {
+                    throw new MyArrayDataException("Неверные данные в элементе:" + i  + "," + j);
+                }
+            }
+        }
+        return sum;
+    }
+
     public static void main(String[] args) {
-        String[][] arr = new String[][]
-                {{"1", "2", "3", "4"},
-                {"1", "1", "1", "1"},
-                {"2", "2", "3", "3"},
-                {"4", "3", "3", "4"}};
+
+        String[][] successArray =
+                {{"1", "2", "3", "4"}, {"0", "1", "2", "3", "4"}, {"1", "2", "3", "4"}, {"1", "2", "3", "4"}};
+
+        Integer[][] array = new Integer[4][];
+        array[0] = new Integer[5];
+        array[0] = new Integer[4];
+        array[0] = new Integer[4];
 
         try {
-            try {
-                int result = method(arr);
-                System.out.println(result);
-            } catch (MyArraySizeException e) {
-                System.out.println("Массив другого размера");
-            }
+            System.out.println(arraySum(successArray));
+        } catch (MyArraySizeException e) {
+            e.printStackTrace();
+        } catch (MyArrayDataException e) {
+            e.printStackTrace();
         }
-        catch (MyArrayDataException e) {
-            System.out.println("Неверное значение");
-            System.out.println("Ошибка в ячейке: " + e.i + "x" + e.j);
+
+        String[][] invalidDataArray =
+                {{"2", "2", "2", "2"}, {"3", "2", "o", "3"}, {"x", "1", "1", "4"}, {"3", "4", "3", "4"}};
+
+        try {
+            System.out.println(arraySum(invalidDataArray));
+        } catch (MyArraySizeException e) {
+            e.printStackTrace();
+        } catch (MyArrayDataException e) {
+            e.printStackTrace();
         }
-    }
-    public static int method(String[][] arr) throws MyArraySizeException, MyArrayDataException {
-        int count = 0;
-        if (arr.length != 4) {
-            throw new MyArraySizeException();
-        }
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i].length != 4) {
-                throw new MyArraySizeException();
-            }
-            for (int j = 0; j < arr[i].length; j++) {
-                try {
-                    count = count + Integer.parseInt(arr[i][j]);
-                }
-                catch (NumberFormatException e) {
-                    throw new MyArrayDataException(i, j);
-                }
-            }
-        }
-        return count;
     }
 }
-
