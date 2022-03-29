ab2a52fd21421a321f118ca26998a7032dff9504
de11ete <99339619+de11ete@users.noreply.github.com>
2022-03-30 01:21:04 +0400

    Update MyArrayDataException.java

--- lesson2/MyArrayDataException.java
+++ lesson2/MyArrayDataException.java
@@ -1,11 +1,7 @@
 package lesson2;
 
 public class MyArrayDataException extends Exception {
-    public int i;
-    public int j;
-
-    MyArrayDataException(int i, int j) {
-        this.i = i;
-        this.j = j;
+    public MyArrayDataException(String message) {
+        super(message);
     }
 }
